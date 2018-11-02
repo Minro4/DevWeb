ebe371f035b4dbbc8ede80e2b34d354d06befa3c
William O'Sullivan-Dargis <william19.od@gmail.com>
2018-11-02 16:47:08 -0400

    Jai fait le sort

--- javascript/accueil.js
+++ javascript/accueil.js
@@ -3,11 +3,16 @@ var data=[{"#":1,"Activité":"Natation","Responsable":"Michel Provencher","Nombr
 			  {"#":3,"Activité":"Randonnée","Responsable":"Catherine Pelletier","Nombre d'inscrits":10},
 			  {"#":4,"Activité":"Kayak","Responsable":"Josée Coté","Nombre d'inscrits":14},
 			  {"#":5,"Activité":"Velo","Responsable":"Jean-Yves Surroy","Nombre d'inscrits":22},
-			  {"#":6,"Activité":"Echecs","Responsable":"Emilie Simard","Nombre d'inscrits":11}];
+              {"#":6,"Activité":"Echecs","Responsable":"Emilie Simard","Nombre d'inscrits":11}];
+
+var table;
 
 
 $(document).ready(function() {
     fillTable();
+    $("th").click(function() {
+        sortTable(event.target.x);
+      });
 });
 
 function fillTable(){
@@ -22,15 +27,15 @@ function fillTable(){
         }
 
         // Créer la table
-        var table = document.createElement("table");
+        table = document.createElement("table");
 
         // Ajoute les tables headers
-
         var tr = table.insertRow(-1);                   // Ajoute une row
 
         for (var i = 0; i < col.length; i++) {
             var th = document.createElement("th");      // Créé les table headers
-            th.innerHTML = col[i];
+            th.innerHTML =  col[i];
+            th.x = i;
             tr.appendChild(th);
         }
 
@@ -50,3 +55,56 @@ function fillTable(){
         divContainer.innerHTML = "";
         divContainer.appendChild(table);
 }
+function sortTable(n) {
+    var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc"; 
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /*check if the two rows should switch place,
+        based on the direction, asc or desc:*/
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
