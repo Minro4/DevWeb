e759fe4e5e0cc34b29e50f047ae0d30ef5e8dbb8
William O'Sullivan-Dargis <william19.od@gmail.com>
2018-11-08 00:15:19 -0500

    bla bla bla

--- javascript/accueil.js
+++ javascript/accueil.js
@@ -5,12 +5,13 @@ var data=[{"order":1,"activity":"Natation","manager":"Michel Provencher","numofs
 {"order":5,"activity":"Velo","manager":"Jean-Yves Surroy","numofsub":22},
 {"order":6,"activity":"Echecs","manager":"Emilie Simard","numofsub":11}];
 var table;
-var col;
 
 $(document).ready(function() {
-  createFillTable();
-    $("th").on('click', function() {
-        sortData(data,event.target.x);
+    table = $("table");
+    fillTable();
+    $("th").click( function() {
+        sortData(data,event.target.cellIndex);
+        $("tr:not(tr:first-child)").remove(); //Enlève toutes les rows sauf la première
         fillTable();
       });
     $("#accueilMenu,#inscrireMenu,#carteMenu").click(function(event) {
@@ -18,46 +19,19 @@ $(document).ready(function() {
       });
 
 
-function createFillTable(){
-        //ajoute les propriétés de data 
-        col = [];
-        for (var i = 0; i < data.length; i++) {
-            for (var key in data[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }
-        table = $("table");
-        // Ajoute l'information de data
+function fillTable(){     
         for (var i = 0; i < data.length; i++) {     //loop dans toutes les rows
-
-            tr = table.insertRow(-1);
-            
-            for (var j = 0; j < col.length; j++) {  //loop dans touts les colonnes
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = data[i][col[j]];
-            }
+            tr = table.append("<tr><td>"+ data[i]["order"] +"</td><td>"+ data[i]["activity"] +"</td><td>"+ data[i]["manager"] +"</td><td>"+ data[i]["numofsub"] +"</td></tr>");
         }
-
-        // Ajoute la table au document HTML
-        var divContainer = document.getElementById("table");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
-}
-
-function fillTable(){
-  // Ajoute l'information de data
-  for (var i = 0; i < data.length; i++) {     //loop dans toutes les rows
-     for (var j = 0; j < col.length; j++) {  //loop dans touts les colonnes
-         table.rows[i+1].getElementsByTagName("TD")[j].innerHTML = data[i][col[j]];
-     }
- }
 }
 
 var lastCol = 0;    //se souvient de la dernière colonne qui à été mis en ordre
 function sortData(a, colIndex){
-  if (colIndex === lastCol){    //si on a clické 2 fois sur la même colonne, on les met en ordre descendant
+    if (data.length == 0) 
+        return;
+
+    var key =  Object.keys(data[0])[colIndex];  //Permet d'avoir le nom de la propriété que l'utilisateur souhaite ordonner
+    if (colIndex === lastCol){    //si on a clické 2 fois sur la même colonne, on les met en ordre descendant
       a.sort(sortFunctionDesc);
       lastCol = -1;
     }
@@ -65,21 +39,21 @@ function sortData(a, colIndex){
       a.sort(sortFunctionAsc);
       lastCol = colIndex;
     }
- 
+
   function sortFunctionAsc(a, b) {
-      if (a[col[colIndex]] === b[col[colIndex]]) {
+      if (a[key] === b[key]) {
           return 0;
       }
       else {
-          return (a[col[colIndex]] < b[col[colIndex]]) ? -1 : 1;         
+          return (a[key] < b[key]) ? -1 : 1;         
       }
   }
   function sortFunctionDesc(a, b) {
-    if (a[col[colIndex]] === b[col[colIndex]]) {
+    if (a[key] === b[key]) {
         return 0;
     }
     else {
-        return (a[col[colIndex]] < b[col[colIndex]]) ? 1 : -1;
+        return (a[key] < b[key]) ? 1 : -1;
     }
 }
 
@@ -100,16 +74,4 @@ function affichage(affiche){
         $("#carte").css({"display" : "block"});
     }
 }
-
-//Début des fonctions inutiles
-function removeTable(){
-  $("table").remove();
-}
-function unfillTable(){
-  var tableData = $("TD");
-  for (var i = 0; i < tableData.length; i++) {
-    tableData[i].innerHTML = "";
-  }
-}
-//fin des fonctions inutiles
 });
