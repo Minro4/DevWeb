95a0b77e018633ad7696424b660f88fa4f8ce3f5
Unknown <aseriam123@hotmail.com>
2018-11-12 18:26:43 -0500

    affichage cacacacacacacacacaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- javascript/accueil.js
+++ javascript/accueil.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
         affichage(event.target.id);
       });
          //vef de submit
-   $("form").submit(function(event)
+    $("form").submit(function(event)
    {
        var xd = $("input");
        var prob = "LES CHAMPS OBLIGATOIRES CI-DESSOUS N'ONT PAS ÉTÉ REMPLIS\n";
@@ -50,59 +50,70 @@ $(document).ready(function() {
    });
 
 
-function fillTable(){     
-        for (var i = 0; i < data.length; i++) {     //loop dans toutes les rows
-            tr = table.append("<tr><td>"+ data[i]["order"] +"</td><td>"+ data[i]["activity"] +"</td><td>"+ data[i]["manager"] +"</td><td>"+ data[i]["numofsub"] +"</td></tr>");
+    function fillTable(){
+            for (var i = 0; i < data.length; i++) {     //loop dans toutes les rows
+                tr = table.append("<tr><td>"+ data[i]["order"] +"</td><td>"+ data[i]["activity"] +"</td><td>"+ data[i]["manager"] +"</td><td>"+ data[i]["numofsub"] +"</td></tr>");
+            }
+    }
+
+    var lastCol = 0;    //se souvient de la dernière colonne qui à été mis en ordre
+    function sortData(a, colIndex){
+        if (data.length == 0)
+            return;
+
+        var key =  Object.keys(data[0])[colIndex];  //Permet d'avoir le nom de la propriété que l'utilisateur souhaite ordonner
+        if (colIndex === lastCol){    //si on a clické 2 fois sur la même colonne, on les met en ordre descendant
+          a.sort(sortFunctionDesc);
+          lastCol = -1;
         }
-}
+        else{
+          a.sort(sortFunctionAsc);
+          lastCol = colIndex;
+        }
 
-var lastCol = 0;    //se souvient de la dernière colonne qui à été mis en ordre
-function sortData(a, colIndex){
-    if (data.length == 0) 
-        return;
-
-    var key =  Object.keys(data[0])[colIndex];  //Permet d'avoir le nom de la propriété que l'utilisateur souhaite ordonner
-    if (colIndex === lastCol){    //si on a clické 2 fois sur la même colonne, on les met en ordre descendant
-      a.sort(sortFunctionDesc);
-      lastCol = -1;
-    }
-    else{
-      a.sort(sortFunctionAsc);
-      lastCol = colIndex;
-    }
-
-  function sortFunctionAsc(a, b) {
-      if (a[key] === b[key]) {
-          return 0;
+      function sortFunctionAsc(a, b) {
+          if (a[key] === b[key]) {
+              return 0;
+          }
+          else {
+              return (a[key] < b[key]) ? -1 : 1;
+          }
       }
-      else {
-          return (a[key] < b[key]) ? -1 : 1;         
-      }
-  }
-  function sortFunctionDesc(a, b) {
-    if (a[key] === b[key]) {
-        return 0;
+      function sortFunctionDesc(a, b) {
+        if (a[key] === b[key]) {
+            return 0;
+        }
+        else {
+            return (a[key] < b[key]) ? 1 : -1;
+        }
     }
-    else {
-        return (a[key] < b[key]) ? 1 : -1;
-    }
-}
 
-  return a;
-}
+      return a;
+    }
 
-function affichage(affiche){
-    if(affiche == "accueilMenu"){
-        $("article").css({"display" : "none"});
-        $("#accueil").css({"display" : "block"});
+
+    $("li").click(function(){
+        $("li").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    function affichage(affiche){
+        if(affiche == "accueilMenu" && $("#accueil").is(":hidden")){
+            $("article:visible").slideUp(function(){
+                $("#accueil").slideDown();
+            });
+
+        }
+        if(affiche =="inscrireMenu" && $("#inscription").is(":hidden")){
+            $("article:visible").slideUp(function(){
+                $("#inscription").slideDown();
+            });
+
+        }
+        if(affiche == "carteMenu" && $("#carte").is(":hidden")){
+            $("article:visible").slideUp(function(){
+                $("#carte").slideDown();
+            });
+
+        }
     }
-    if(affiche =="inscrireMenu"){
-        $("article").css({"display" : "none"});
-        $("#inscription").css({"display" : "block"});
-    }
-    if(affiche == "carteMenu"){
-        $("article").css({"display" : "none"});
-        $("#carte").css({"display" : "block"});
-    }
-}
 });
