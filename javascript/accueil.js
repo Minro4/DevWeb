6803908deebe9abeb5d97e1cf6b314fe8d14d837
William O'Sullivan-Dargis <william19.od@gmail.com>
2018-11-20 19:15:50 -0500

    gf

--- javascript/accueil.js
+++ javascript/accueil.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
         $(this).addClass("selected");
     });
 
-         //vef de submit
+    //Fait la vérification du form
     $("form").submit(function(event)
    {
        var xd = $("input");
@@ -70,7 +70,7 @@ $(document).ready(function() {
         if (data.length == 0)
             return;
 
-        var key =  Object.keys(data[0])[colIndex];  //Permet d'avoir le nom de la propriété que l'utilisateur souhaite ordonner
+        var key =  Object.keys(data[0])[colIndex];  //Permet d'avoir le nom de la propriété que l'utilisateur souhaite trier
         if (colIndex === lastCol){    //si on a clické 2 fois sur la même colonne, on les met en ordre descendant
           a.sort(sortFunctionDesc);
           lastCol = -1;
@@ -80,7 +80,7 @@ $(document).ready(function() {
           lastCol = colIndex;
         }
 
-      function sortFunctionAsc(a, b) {  //sort ascendant
+      function sortFunctionAsc(a, b) {  //trie ascendant
           if (a[key] === b[key]) {
               return 0;
           }
@@ -88,7 +88,7 @@ $(document).ready(function() {
               return (a[key] < b[key]) ? -1 : 1;
           }
       }
-      function sortFunctionDesc(a, b) { //sort descendant
+      function sortFunctionDesc(a, b) { //trie descendant
         if (a[key] === b[key]) {
             return 0;
         }
@@ -100,9 +100,10 @@ $(document).ready(function() {
       return a;
     }
 
+    //Fonction qui permet de changer quel fenêtre est affiché dans le contenu
     function affichage(affiche){
-        if(affiche == "accueilMenu" && $("#accueil").is(":hidden")){
-            $("article:visible").slideUp(function(){
+        if(affiche == "accueilMenu" && $("#accueil").is(":hidden")){    //affiche l'accueil sauf si il est déjà affiché
+            $("article:visible").slideUp(function(){    //slideUp l'article qui est affiché et slidedown accueil lorsque que l'article affiché a terminé son slideUp
                 $("#accueil").slideDown();
             });
 
